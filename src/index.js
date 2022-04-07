c4acd3e004fcefab636af212e86903d0645cea8e
Adam DePaolo <amdepaolo@gmail.com>
2022-04-07 19:57:28 -0400

    main deliverables met

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    buildToDos(e.target.new_task_description.value);
+    document.querySelector('form').reset()
+  })
+  
 });
+
+function buildToDos(toDo){
+  let newItem = document.createElement('li')
+  let button = document.createElement('button')
+  button.addEventListener('click', (e)=> e.target.parentNode.remove())
+  button.innerText = ' Did it! ';
+  newItem.innerText = ' ' + toDo + ' ';
+  newItem.appendChild(button);
+  document.querySelector('#list').appendChild(newItem);
+}
