0319780fb59827eb6b500a8c24ef9e4237b861b9
Adam DePaolo <amdepaolo@gmail.com>
2022-04-11 15:46:28 -0400

    added priority selector

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('form').addEventListener('submit', (e)=>{
     e.preventDefault()
     buildToDos(e.target.new_task_description.value);
+    console.log(e.target.new_task_description.value)
+    console.log(document.querySelector("#priority-selector").value)
     document.querySelector('form').reset()
   })
   
 });
 
 function buildToDos(toDo){
-  let newItem = document.createElement('li')
-  let button = document.createElement('button')
+  let newItem = document.createElement('li');
+  let button = document.createElement('button');
+  let priorityLevel = document.getElementById("priority-selector").value;
   button.addEventListener('click', (e)=> e.target.parentNode.remove())
   button.innerText = ' Did it! ';
-  newItem.innerText = ' ' + toDo + ' ';
+  if (priorityLevel === "high"){
+    newItem.innerText = ' ' + toDo + ' ';
+    document.querySelector('#high-priority-tasks').appendChild(newItem);
+  };
+  if (priorityLevel === "medium" || priorityLevel ==="none"){
+    newItem.innerText = ' ' + toDo + ' ';document.querySelector('#tasks').appendChild(newItem);
+  };
+  if (priorityLevel === "low"){
+    newItem.innerText = ' ' + toDo + ' ';
+    document.querySelector('#low-priority-tasks').appendChild(newItem);
+  };
   newItem.appendChild(button);
-  document.querySelector('#list').appendChild(newItem);
+  
 }
